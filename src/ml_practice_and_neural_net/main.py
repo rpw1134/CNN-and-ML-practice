88c9e388496ccd0c416905c1d6296f44a78054f8
rpw1134 <ryanw1134@gmail.com>
2026-01-12 21:44:07 -0500

    trying to figure out modules

--- src/ml_practice_and_neural_net/main.py
+++ src/ml_practice_and_neural_net/main.py
@@ -1,5 +1,5 @@
 def main():
-    print("Hello from ml-practice-and-neural-net!")
+    print("Hello from ml_practice_and_neural_net!")
 
 
 if __name__ == "__main__":
---
+++ src/ml_practice_and_neural_net/ml_practice/tester.py
@@ -0,0 +1,3 @@
+from ..ml_practice_and_neural_net.cnn import print_hello
+print("Hello World")
+print_hello()
