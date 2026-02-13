f4c7a97e990f04e9ffd84b1d2a1bf830a02a3677
rpw1134 <ryanw1134@gmail.com>
2026-02-13 18:07:30 -0500

    cross validator complete

--- src/ml_practice_and_neural_net/ml_practice/models/__init__.py
+++ src/ml_practice_and_neural_net/ml_practice/models/__init__.py
@@ -3,11 +3,13 @@ from .LogisticRegressionModel import LogisticRegressionModel
 from .SoftmaxRegressionModel import SoftmaxRegressionModel
 from .LinearRegressionModel import LinearRegressionModel
 
-__all__ = ['BaseModel', 'LogisticRegressionModel', 'SoftmaxRegressionModel', 'LinearRegressionModel']
 
 model_map = {
     "linear": LinearRegressionModel,
     "logistic": LogisticRegressionModel,
     "softmax": SoftmaxRegressionModel
 }
+
+__all__ = ['BaseModel', 'LogisticRegressionModel', 'SoftmaxRegressionModel', 'LinearRegressionModel', 'model_map']
+
 
--- src/ml_practice_and_neural_net/ml_practice/optimization/CrossValidator.py
+++ src/ml_practice_and_neural_net/ml_practice/optimization/CrossValidator.py
@@ -1,6 +1,8 @@
 from ml_practice_and_neural_net.ml_practice.data_classes.Hyperparameters import Hyperparameters
 from ml_practice_and_neural_net.ml_practice.data_classes.ModelData import ModelData
 from ..models import model_map
+import numpy as np
+from numpy.typing import NDArray
 
 
 class CrossValidator:
@@ -11,10 +13,66 @@ class CrossValidator:
     def __init__(self, model: ModelData, hyperparams: Hyperparameters, k: int):
         self.model = model_map[model.model_name](hyperparams)
         self.hyperparams = hyperparams
-        self.data = model.given_data
-        self.labels = model.labels
+        self.data: NDArray = model.given_data
+        self.labels: NDArray = model.labels
         self.k = k
         self.errors = []
+        self.mean_error = None
         self.accuracies = []
-        self.std_devs = []
+        self.mean_accuracy = None
+        self.std_dev = None
 
+    def cross_validate(self):
+        """
+        Perform k-fold cross-validation on the given model and dataset.
+
+        :return: The instance, now with populated error and accuracy metrics for each fold.
+        """
+        n_samples = self.data.shape[0]
+        fold_size = n_samples // self.k
+        # indices for each fold
+        indices = [[i * fold_size, (i + 1) * fold_size] for i in range(self.k)]
+        # add remainder to the last fold
+        indices[-1][1] += n_samples % self.k
+        for i in range(self.k):
+            # split train and test
+            test_samples = self.data[indices[i][0]:indices[i][1]]
+            test_labels = self.labels[indices[i][0]:indices[i][1]]
+            train_samples = self.data[indices[i][1]:] if i == 0 else np.concatenate((self.data[:indices[i][0]], self.data[indices[i][1]:]), axis=0)
+            train_labels = self.labels[indices[i][1]:] if i == 0 else np.concatenate((self.labels[:indices[i][0]], self.labels[indices[i][1]:]), axis=0)
+
+            self.model.fit(train_samples, train_labels)
+            predictions = self.model.predict(test_samples)
+            error = np.mean((predictions - test_labels) ** 2)
+            accuracy = np.mean(predictions == test_labels)
+            self.errors.append(error)
+            self.accuracies.append(accuracy)
+        return self
+
+
+    def get_metrics_mean_metrics(self):
+        """
+        Calculate and return the mean error, mean accuracy, and standard deviation of errors across all folds, or return them if previously calculated.
+        :return: A dictionary containing the mean error, mean accuracy, and standard deviation of errors across all folds.
+        """
+        if self.mean_error is None:
+            self.mean_error = sum(self.errors) / len(self.errors)
+        if self.mean_accuracy is None:
+            self.mean_accuracy = sum(self.accuracies) / len(self.accuracies)
+        if self.std_dev is None:
+            self.std_dev = (sum((x - self.mean_error) ** 2 for x in self.errors) / len(self.errors)) ** 0.5
+        return {
+            "mean_error": self.mean_error,
+            "mean_accuracy": self.mean_accuracy,
+            "std_dev": self.std_dev
+        }
+
+    def get_errors_and_accuracies(self):
+        """
+        Return the list of errors and accuracies for each fold.
+        :return: A dictionary containing the list of errors and accuracies for each fold.
+        """
+        return {
+            "errors": self.errors,
+            "accuracies": self.accuracies
+        }
