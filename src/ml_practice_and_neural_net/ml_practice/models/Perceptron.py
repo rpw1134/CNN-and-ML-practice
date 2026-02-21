58558bd682e0125f9b2138c89cba2d30daac8126
rpw1134 <ryanw1134@gmail.com>
2026-02-21 16:21:54 -0500

    perceptron fitting

--- src/ml_practice_and_neural_net/ml_practice/models/Perceptron.py
+++ src/ml_practice_and_neural_net/ml_practice/models/Perceptron.py
@@ -1,13 +1,24 @@
+from ml_practice_and_neural_net.ml_practice.data_management.general import add_data_bias_term
 from ml_practice_and_neural_net.ml_practice.models import BaseModel
 import numpy as np
 from numpy.typing import NDArray
 
 
 class Perceptron(BaseModel):
-
+    # only uses epochs and learning rate, no regularization or training method
     def fit(self, X: NDArray, y: NDArray):
-        raise NotImplementedError("Perceptron model is not implemented yet.")
-
+        if X.ndim != 2:
+            X = X.reshape(-1, 1)
+        X = add_data_bias_term(X)
+        if y.ndim != 2:
+            y = y.reshape(-1, 1)
+        self.weights = np.random.uniform(low=-1, high=1, size=(X.shape[1], 1))
+        for epoch in range(self.training_iterations):
+            predictions = np.sign(X @ self.weights)
+            misclassified = (predictions != y).flatten()
+            if not np.any(misclassified):
+                break
+            self.weights += self.learning_rate * (X[misclassified].T @ y[misclassified])
 
 
     def predict(self, X):
