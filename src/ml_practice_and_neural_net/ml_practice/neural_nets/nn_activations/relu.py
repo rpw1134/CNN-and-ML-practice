4f33027e42dd6a1e507e0b88e9db20474eb100c0
rpw1134 <ryanw1134@gmail.com>
2026-02-22 18:49:43 -0500

    relu "

---
+++ src/ml_practice_and_neural_net/ml_practice/neural_nets/nn_activations/relu.py
@@ -0,0 +1,15 @@
+from typing import Tuple
+
+import numpy as np
+from typing import Tuple, Callable
+from numpy.typing import NDArray
+
+def build() -> Tuple[Callable, Callable]:
+
+    def activate(x: NDArray) -> NDArray:
+        return np.maximum(0, x)
+
+    def activation_derivative(x: NDArray) -> NDArray:
+        return np.where(x > 0, 1, 0)
+
+    return activate, activation_derivative
