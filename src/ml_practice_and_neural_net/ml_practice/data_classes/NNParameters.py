46055ceea6b8d74ce246f6033f21d17e3238d3a6
rpw1134 <ryanw1134@gmail.com>
2026-02-22 19:36:01 -0500

    just sparse file. might combine into a single layer

--- src/ml_practice_and_neural_net/ml_practice/data_classes/NNParameters.py
+++ src/ml_practice_and_neural_net/ml_practice/data_classes/NNParameters.py
@@ -6,3 +6,4 @@ class NNLayerParameters:
     input_dim: int = 16
     activation: str = "relu"    # Options could include "relu", "sigmoid", "tanh", "leaky_relu", etc. for different activation functions
     weight_init: str = "xavier" # Options could include "xavier", "he", "random", etc. for different weight initialization strategies
+    sparsity: float = 1.0       # Fraction of connections to keep (1.0 = dense, 0.5 = 50% connected). Only used by SparseLayer
