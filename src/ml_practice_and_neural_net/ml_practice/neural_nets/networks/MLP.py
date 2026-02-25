0f094cc81f6569aad6911a653387388ee7d372c2
rpw1134 <ryanw1134@gmail.com>
2026-02-24 23:24:28 -0500

    will need to do loss functions for nn

--- src/ml_practice_and_neural_net/ml_practice/neural_nets/networks/MLP.py
+++ src/ml_practice_and_neural_net/ml_practice/neural_nets/networks/MLP.py
@@ -4,6 +4,10 @@ from ml_practice_and_neural_net.ml_practice.data_classes.NNHyperparameters impor
 from ml_practice_and_neural_net.ml_practice.data_classes.NNLayerParameters import NNLayerParameters
 from ml_practice_and_neural_net.ml_practice.data_classes.NNParameters import NNParameters
 from ml_practice_and_neural_net.ml_practice.loss import loss_map
+from ..nn_layers.Layer import Layer
+
+import numpy as np
+from numpy.typing import NDArray
 
 
 class MLP:
@@ -15,12 +19,27 @@ class MLP:
     # training should: split into 80/20 train/validate, pass training forward, pass backwards, compute validate error
     # if at any point validation doesn't increase for to many iterations, stop training and return the model with the best validation error (need to track best weights and biases)
 
-
     def __init__(self, layer_configs: List[NNLayerParameters], hyperparameters: NNHyperparameters, params: NNParameters):
-        self.layers = layer_configs
+        self.layers = [Layer(config) for config in layer_configs]
         self.learning_rate = hyperparameters.learning_rate
         self.num_iterations = hyperparameters.num_iterations
         self.regularization_strength = hyperparameters.regularization_strength
         self.regularization_type = hyperparameters.regularization_type
         self.batch_size = hyperparameters.batch_size
+
+        # I think I need to reconsider this, as the layer itself outputs a prediction. We are no longer doing loss with respect to weights
         self.loss_builder = loss_map[params.loss]
+
+    def feed_forward(self, X: NDArray) -> NDArray:
+        output = X
+        for layer in self.layers:
+            output = layer.feed_forward(output)
+        return output
+
+    def predict(self, X: NDArray) -> NDArray:
+        output = self.feed_forward(X)
+        return output
+
+    def compute_loss(self, predictions: NDArray, Y: NDArray) -> float:
+
+        pass
